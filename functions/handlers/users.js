f9d6f46e16060639b5d80cb817cf9497e965aed9
benjflowers <benjaminjflowers@gmail.com>
2021-05-29 14:23:57 -0500

    image uploading

--- functions/handlers/users.js
+++ functions/handlers/users.js
@@ -1,6 +1,7 @@
 const { admin, db } = require("../util/admin");
 
 const config = require("../util/config");
+const { uuid } = require("uuidv4");
 
 const firebase = require("firebase");
 firebase.initializeApp(config);
@@ -20,6 +21,7 @@ exports.signup = (req, res) => {
 
   if (!valid) return res.status(400).json(errors);
 
+  const noImg = "no-img.png";
   let token, userId;
   db.doc(`/users/${newUser.handle}`)
     .get()
@@ -42,6 +44,7 @@ exports.signup = (req, res) => {
         handle: newUser.handle,
         email: newUser.email,
         createdAt: new Date().toISOString(),
+        imageUrl: `https://firebasestorage.googleapis.com/v0/b/${config.storageBucket}/o/${noImg}?alt=media`,
         userId: userId,
       };
 
@@ -91,4 +94,69 @@ exports.login = (req, res) => {
           .json({ general: "Wrong credentials, please try again" });
       } else return res.status(500).json({ error: err.code });
     });
+};
+
+exports.uploadImage = (req, res) => {
+  const BusBoy = require("busboy");
+  const path = require("path");
+  const os = require("os");
+  const fs = require("fs");
+
+  const busboy = new BusBoy({ headers: req.headers });
+
+  let imageFileName;
+  let imageToBeUploaded = {};
+  // String for image token
+  let generatedToken = uuid();
+
+  busboy.on("file", (fieldname, file, filename, encoding, mimetype) => {
+    // my.image.png
+    if (
+      mimetype !== "image/jpeg" &&
+      mimetype !== "image/png" &&
+      mimetype !== "image/jpg"
+    ) {
+      return res
+        .status(400)
+        .json({
+          error:
+            "Wrong file type submitted, accepted types are: jpeg/jpg, or png",
+        });
+    }
+
+    const imageExtension = filename.split(".")[filename.split(".").length - 1];
+    imageFileName = `${Math.round(
+      Math.random() * 1000000000000
+    ).toString()}.${imageExtension}`;
+    const filepath = path.join(os.tmpdir(), imageFileName);
+    imageToBeUploaded = { filepath, mimetype };
+    file.pipe(fs.createWriteStream(filepath));
+  });
+  busboy.on("finish", () => {
+    admin
+      .storage()
+      .bucket()
+      .upload(imageToBeUploaded.filepath, {
+        resumable: false,
+        metadata: {
+          metadata: {
+            contentType: imageToBeUploaded.mimetype,
+            //Generate token to be appended to image Url
+            firebaseStorageDownloadTokens: generatedToken,
+          },
+        },
+      })
+      .then(() => {
+        const imageUrl = `https://firebasestorage.googleapis.com/v0/b/${config.storageBucket}/o/${imageFileName}?alt=media`;
+        return db.doc(`/users/${req.user.handle}`).update({ imageUrl });
+      })
+      .then(() => {
+        return res.json({ message: "Image uploaded successfully" });
+      })
+      .catch((err) => {
+        console.error(err);
+        return res.status(500).json({ error: err.code });
+      });
+  });
+  busboy.end(req.rawBody);
 };
