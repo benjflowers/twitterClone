c925bf8247899d8e846153719a2d9f7bc4accd52
benjflowers <benjaminjflowers@gmail.com>
2021-05-30 14:26:19 -0500

    delete scream, like scream, unlike screame

--- functions/index.js
+++ functions/index.js
@@ -7,6 +7,9 @@ const {
   postOneScream,
   getScream,
   postComment,
+  likeScream,
+  unlikeScream,
+  deleteScream,
 } = require("./handlers/screams");
 const {
   signup,
@@ -21,6 +24,9 @@ app.get("/screams", getAllScreams);
 app.post("/scream", FBAuth, postOneScream);
 app.get("/scream/:screamId", getScream);
 app.post("/scream/:screamId/comment", FBAuth, postComment);
+app.get("/scream/:screamId/like", FBAuth, likeScream);
+app.get("/scream/:screamId/unlike", FBAuth, unlikeScream);
+app.delete("/scream/:screamId", FBAuth, deleteScream);
 //TODO:
 // delete scream
 // like scream
