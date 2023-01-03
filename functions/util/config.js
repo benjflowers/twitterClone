e46476ba732e11f7ea3488f89aadc14d28e94b94
Ben Flowers <40609699+benjflowers@users.noreply.github.com>
2023-01-03 11:45:29 -0600

    Delete config.js

--- functions/util/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  apiKey: "AIzaSyD0XiFe6DmU8QrrRnJk2TXimi_bJGmouAc",
-  authDomain: "socialape-1dffa.firebaseapp.com",
-  projectId: "socialape-1dffa",
-  storageBucket: "socialape-1dffa.appspot.com",
-  messagingSenderId: "317481205854",
-  appId: "1:317481205854:web:b5a8911935d4b3387af4cf",
-  measurementId: "G-B87NV2L9DR",
-};
